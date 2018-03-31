561d234078577ec25b16ac5219b67b63c1fa0041
hufei <591541804@qq.com>
2018-03-31 11:13:18 +0800

    阿德

--- src/index.js
+++ src/index.js
@@ -137,5 +137,33 @@ function timeout(ms) {
     console.log(value);
   }
   
+  console.log(Promise.resolve());
 
   asyncPrint('hello world', 3000);
+  class RangeIterator {
+    constructor(start, stop) {
+      this.value = start;
+      this.stop = stop;
+    }
+  
+    [Symbol.iterator]() { return this; }
+  
+    next() {
+      var value = this.value;
+      if (value < this.stop) {
+        this.value++;
+        return {done: false, value: value};
+      }
+      return {done: true, value: undefined};
+    }
+  }
+  
+  function range(start, stop) {
+    let obj = new RangeIterator(start, stop);
+    console.log(obj)
+    return obj    
+}
+  
+  for (var value of range(0, 3)) {
+    console.log(value); // 0, 1, 2
+  }
